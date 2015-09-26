c15d512903291393ebd5fa16942ad2944df18af0
gdsanders <gd@digitalequity.com>
2015-09-25 19:48:23 -0500

    NumbersTapped UI actions

--- CalcU/ViewController.swift
+++ CalcU/ViewController.swift
@@ -24,10 +24,72 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // Calculator button actions
+    
     
     @IBAction func decimalTapped(sender: UIButton) {
         
     }
+    
+    
+    @IBAction func zeroTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func oneTapped(sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func twoTapped(sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func threeTapped(sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func fourTapped(sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func fiveTapped(sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func sixTapped(sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func sevenTapped(sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func eightTapped(sender: UIButton) {
+        
+    }
+    
+    @IBAction func nineTapped(sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 
